c432573f74fc5e64f4e0d337d35d8679602297ac
Denis Parashenko <denus.parashencko@gmail.com>
2024-07-20 21:34:26 +0300

    Edit Task_2.

--- Lesson_1/Task_2/main.cpp
+++ Lesson_1/Task_2/main.cpp
@@ -4,18 +4,11 @@ using namespace std;
 
 int main() {
     float data_1,data_2;
-    char operations;
     cout << "Enter number_1." <<endl;
     cin >> data_1;
-    cout << "Select operations." <<endl;
-    cin >> operations;
     cout << "Enter number_2." <<endl;
     cin >> data_2;
-    if (operations == '+'){
-        cout << "Result: " << data_1 + data_2 << endl;
-    }/*else if (operations == 'Operation name') {
+    cout << "Result: " << data_1 + data_2 << endl;
 
-    }
-*/
     return 0;
 }
