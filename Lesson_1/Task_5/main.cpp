ddda5e9bd97474de016edd5292f48f1b294eb656
Denis Parashenko <denus.parashencko@gmail.com>
2024-07-20 21:24:43 +0300

    Removing unnecessary files

---
+++ Lesson_1/Task_5/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void QuadraticEquation(double a,double b, double c){
+    if (a == 0){
+        cout << "Not Quadratic Equation." << endl;
+    }
+
+    return;
+}
+
+int main()
+{
+    double a,b,c;
+    cout << "Hello World!" << endl;
+    return 0;
+}
