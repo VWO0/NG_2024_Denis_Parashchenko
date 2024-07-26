48a4e5091905a756064a311a14f0c887be9a32fd
Denis Parashenko <denus.parashencko@gmail.com>
2024-07-26 18:30:55 +0300

    Removed unnecessary files. Corrected errors in the code (Task_1 - Task_4).

--- Lesson_1/Task_3/main.cpp
+++ Lesson_1/Task_3/main.cpp
@@ -1,31 +1,30 @@
 #include <iostream>
-
 using namespace std;
-int money,million_check = 1000000;
-void checkLowSalary(int money);
-void checkHighSalary(int money);
 
-void checklowsalary(int money) {
-    if (money < 1000){
+int money, million_check = 1000001;
+
+void checkLowSalary(int money) {
+    if (1000 - money > 0) {
         cout << "Work harder!" << endl;
     }
 }
 
-void checkhighsalary(int money) {
-    if (million_check < money) {
+void checkHighSalary(int money) {
+    if (money - 1000000 > 0) {
         cout << "Yes, you are a millionaire!" << endl;
     }
-    if (million_check > money) {
+    if (1000000 - money >= 0 && money - 1000 >= 0) {
         cout << "You're doing a good job!" << endl;
     }
 }
 
-int main()
-{
-
-    cout << "How much do you earn?" <<endl;
+int main() {
+    cout << "How much do you earn?" << endl;
     cin >> money;
-    checklowsalary(money);
 
+    checkLowSalary(money);
+    checkHighSalary(money);
+
+    cout << "But you’re great!" << endl;
     return 0;
 }
--- Lesson_1/Task_4/main.cpp
+++ Lesson_1/Task_4/main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
+#include <cmath> // Для функции sqrt
 
 using namespace std;
 
-int main()
-{
-    float result,data_1,data_2;
+int main() {
+    float result, data_1, data_2;
     char operations;
-    cout << "Enter number_1." <<endl;
+
+    cout << "Enter number_1." << endl;
     cin >> data_1;
-    cout << "Select operations." <<endl;
+
+    cout << "Select operations: +,-,*,/,s (square), r (square root)" << endl;
     cin >> operations;
-    cout << "Enter number_2." <<endl;
-    cin >> data_2;
+
+    if (operations != 's' && operations != 'r') {
+        cout << "Enter number_2." << endl;
+        cin >> data_2;
+    }
 
     switch (operations) {
     case '+': {
@@ -31,6 +36,19 @@ int main()
             result = data_1 / data_2;
         } else {
             cout << "Error: Division by zero." << endl;
+            return 1;
+        }
+        break;
+    }
+    case 's': { // Квадрат числа
+        result = data_1 * data_1;
+        break;
+    }
+    case 'r': { // Квадратный корень числа
+        if (data_1 >= 0) {
+            result = sqrt(data_1);
+        } else {
+            cout << "Error: Cannot calculate square root of a negative number." << endl;
             return 1;
         }
         break;
